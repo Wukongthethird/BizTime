8c47ce17caf3cbef70733b9f85d8b48f98b51458
ZeeTom <zthom13@hotmail.com>
2021-07-29 17:32:28 -0600

    added post and put routes for /invoices

--- routes/companies.js
+++ routes/companies.js
@@ -1,4 +1,4 @@
-"use strict"
+"use strict";
 const express = require("express");
 const { NotFoundError, BadRequestError } = require("../expressError");
 
@@ -8,94 +8,94 @@ const router = new express.Router();
 
 /* Returns list of companies, like {companies: [{code, name}, ...]} */
 router.get("/", async function (req, res, next) {
-    let result = await db.query(
-        `SELECT code, name
+  let result = await db.query(
+    `SELECT code, name
         FROM companies`
-    );
+  );
 
-    return res.json({ companies: result.rows });
+  return res.json({ companies: result.rows });
 });
 
 /* Return obj of company: {company: {code, name, description}} */
 router.get("/:code", async function (req, res, next) {
-    let result = await db.query(
-        `SELECT code, name, description
+  let result = await db.query(
+    `SELECT code, name, description
         FROM companies
         WHERE code = $1`,
-        [req.params.code]
-    );
-    // if user tries to get a company that doesn't exist, NotFoundError thrown
-    if (!result.rows[0]) {
-        //let them know if the company doesnt exist
-        throw new NotFoundError(`Company not found: ${code}`);
-    }
-    return res.json({ company: result.rows[0] });
+    [req.params.code]
+  );
+  // if user tries to get a company that doesn't exist, NotFoundError thrown
+  if (!result.rows[0]) {
+    //let them know if the company doesnt exist
+    throw new NotFoundError(`Company not found: ${code}`);
+  }
+  return res.json({ company: result.rows[0] });
 });
 
 /* Returns obj of new company: {company: {code, name, description}} */
 router.post("/", async function (req, res, next) {
-    const { code, name, description } = req.body;
-    // if user doesn't include information, BadRequestError thrown
-    if (!code || !name || !description) {
-        throw new BadRequestError();
-    }
+  const { code, name, description } = req.body;
+  // if user doesn't include information, BadRequestError thrown
+  if (!code || !name || !description) {
+    throw new BadRequestError();
+  }
 
-    // If company code already exists in the database, BadRequest Error thrown
-    try {
-        await db.query(
-            `INSERT INTO companies( code, name, description)
+  // If company code already exists in the database, BadRequest Error thrown
+  try {
+    await db.query(
+      `INSERT INTO companies( code, name, description)
           VALUES ($1, $2, $3)
-          RETURNING (code,name, description)`,
-            [code, name, description]
-        );
-    } catch {
-        throw new BadRequestError("That code already exists!");
-    }
-    return res.status(201).json({ company: { code, name, description } });
+          RETURNING code,name, description`,
+      [code, name, description]
+    );
+  } catch {
+    throw new BadRequestError("That code already exists!");
+  }
+  return res.status(201).json({ company: { code, name, description } });
 });
 
 /* Returns update company object: {company: {code, name, description}} */
 router.put("/:code", async function (req, res, next) {
-    const { name, description } = req.body;
+  const { name, description } = req.body;
 
-    // if user doesn't include information, BadRequestError thrown
-    if ( !name || !description) {
-        throw new BadRequestError();
-    }
+  // if user doesn't include information, BadRequestError thrown
+  if (!name || !description) {
+    throw new BadRequestError();
+  }
 
-    let result = await db.query(
-        `UPDATE companies 
+  let result = await db.query(
+    `UPDATE companies 
         SET name = $2,
             description = $3
             WHERE code = $1
-            RETURNING (code,name, description)`,
-        [req.params.code, name, description]
-    );
-    let company = result.rows[0]
-    /* if a user tries to update a company code that does not exist,
+            RETURNING code,name, description`,
+    [req.params.code, name, description]
+  );
+  let company = result.rows[0];
+  /* if a user tries to update a company code that does not exist,
      NotFoundError thrown */
-    if (!company ){
-        throw new NotFoundError(`Company not found: ${code}`);
-    }
-    //safer to return information supply from the db
-    return res.json( {company} );
+  if (!company) {
+    throw new NotFoundError(`Company not found: ${code}`);
+  }
+  //safer to return information supply from the db
+  return res.json({ company });
 });
 
 /* Returns {status: "deleted"} */
 router.delete("/:code", async function (req, res, next) {
-    let result = await db.query(
-        `DELETE FROM companies 
+  let result = await db.query(
+    `DELETE FROM companies 
         WHERE code = $1 
-        RETURNING (code) `,
-        [req.params.code]
-    );
+        RETURNING code `,
+    [req.params.code]
+  );
 
-    /* if a user tries to update a company code that does not exist,
+  /* if a user tries to update a company code that does not exist,
      NotFoundError thrown */
-    if (!result.rows[0]) {
-        throw new NotFoundError("The code entered already does not exist!");
-    }
-    return res.json({ message: "Deleted" });
+  if (!result.rows[0]) {
+    throw new NotFoundError("The code entered already does not exist!");
+  }
+  return res.json({ message: "Deleted" });
 });
 
 module.exports = router;
--- routes/invoices.js
+++ routes/invoices.js
@@ -1,49 +1,83 @@
-"use strict"
+"use strict";
 const express = require("express");
 const { NotFoundError, BadRequestError } = require("../expressError");
 
-
 const db = require("../db");
+// const { routes } = require("../app");
 
 const router = new express.Router();
 
-router.get("/", async function(req ,res, next){
-    let result = await db.query( 
-        `SELECT id, comp_code
+/* Return info on invoices: like {invoices: [{id, comp_code}, ...]} */
+router.get("/", async function (req, res, next) {
+  let result = await db.query(
+    `SELECT id, comp_code
         FROM invoices
         `
-    )
-    let invoices = result.rows
-    return res.json( {invoices} )
+  );
+  let invoices = result.rows;
+  return res.json({ invoices });
+});
 
-} )
+/* Returns obj on given invoice or 404 if not found.
 
-router.get("/:id", async function(req ,res, next){
-
-    let result = await db.query( 
-        `SELECT id, amt, paid, add_date, paid_date
+Returns {invoice: {id, amt, paid, add_date, paid_date, company: {code, name, description}} */
+router.get("/:id", async function (req, res, next) {
+  let result = await db.query(
+    `SELECT id, amt, paid, add_date, paid_date
         FROM invoices
         WHERE id = $1
         `,
-        [req.params.id]
-    )
-    let invoice = result.rows[0]
-    if(!invoice){
-         throw new NotFoundError(`Invoice ${req.params.id} not found`)}
+    [req.params.id]
+  );
+  let invoice = result.rows[0];
+  if (!invoice) {
+    throw new NotFoundError(`Invoice ${req.params.id} not found`);
+  }
 
-    let company = await db.query(
-        `SELECT code, name, description
+  let company = await db.query(
+    `SELECT code, name, description
         FROM companies JOIN invoices  
         ON invoices.comp_code = companies.code
-        WHERE id =$1`,[req.params.id]
-    )
-    
-    invoice.company = company.rows[0]
-    return res.json( {invoices:invoice} )
-} )
+        WHERE id =$1`,
+    [req.params.id]
+  );
 
+  invoice.company = company.rows[0];
+  return res.json({ invoices: invoice });
+});
 
+router.post("/", async function (req, res, next) {
+  let { comp_code, amt } = req.body;
 
+  if (!comp_code || !amt) {
+    // console.log("this error shouldnt be happening");
+    throw new BadRequestError("You sent invalid info.");
+  }
+  let result = await db.query(
+    `
+        INSERT INTO invoices (comp_code, amt)
+        VALUES ($1, $2)
+        RETURNING id, comp_code, amt, paid, add_date, paid_date
+    `,
+    [comp_code, amt]
+  );
 
+  let invoice = result.rows[0];
+  return res.json({ invoice });
+});
+
+router.put("/:id", async function (req, res, next) {
+  let { amt } = req.body;
+  let result = await db.query(
+    `UPDATE invoices
+         SET amt = ($1)
+         WHERE id = $2
+         RETURNING id, comp_code, amt, paid, add_date, paid_date`,
+    [amt, req.params.id]
+  );
+  console.log(result);
+  let invoice = result.rows[0];
+  return res.json({ invoice });
+});
 
 module.exports = router;
